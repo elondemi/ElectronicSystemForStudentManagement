db90fada17ba4dbcd0777b8e5196de1c1135f5ec
EndriitHoda <endrit.hoda@student.uni-pr.edu>
2022-05-23 17:23:23 +0200

    reduce code

--- pages/student/detyrat.php
+++ pages/student/detyrat.php
@@ -3,33 +3,27 @@
  require "../../resources/parts/header_student.php";
 
  $html1 = '
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <title>Studentpage</title>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
         <link href="../../resources/assets/css/student.css" rel="stylesheet">
         <script src="../../resources/assets/js/student.js"></script>
- 
     </head>  
-    <body>';     
-       
-         $html2 ='
-          <h1 style="margin-left:300px;">Detyrat e mia</h1><hr><br>
-          <table id="customers">
-            <tr>
-              <th>Detyra</th>
-              <th>Lënda</th>
-              <th>Semestri</th>
-              
-            </tr>
-          </table>          
- 
-
-    </body>
+<body>';     
+$html2 ='
+  <h1 style="margin-left:300px;">Detyrat e mia</h1><hr><br>
+  <table id="customers">
+    <tr>
+      <th>Detyra</th>
+      <th>Lënda</th>
+      <th>Semestri</th>
+    </tr>
+  </table>          
+</body>
 </html>
 ';
-
     echo $html1;
     render_header();
     echo $html2;
--- pages/student/vleresimet.php
+++ pages/student/vleresimet.php
@@ -7,28 +7,22 @@ $html1 ='<!DOCTYPE html>
         <title>Studentpage</title>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
         <link href="../../resources/assets/css/student.css" rel="stylesheet">
-        <script src="../../resources/assets/js/student.js"></script>
-         
+        <script src="../../resources/assets/js/student.js"></script>  
     </head>  
     <body>';     
        $html2 ='
-  <!-- Tek menyja “Vlerësimet” studenti do të ketë mundësi të shohë vlerësimin që i është bërë për
-secilën detyrë (edhe këtu të implementohen filterat sipas lëndës dhe semestrit). -->
-          
           <h1 style="margin-left:300px;margin: bottom 40px;">Vlerësimet</h1><hr>
           <table id="customers">
             <tr>
-              <th>Detyra</th>
-              <th>Lënda</th>
-              <th>Semestri</th>
-              <th>Vlerësimi</th>
+            <th>Detyra</th>
+            <th>Lënda</th>
+            <th>Semestri</th>
+            <th>Vlerësimi</th>
             </tr>     
           </table>
     </body>
 </html>';
-
 echo $html1;
 render_header();
 echo $html2;
-
 ?>
