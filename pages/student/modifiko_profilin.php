b75d36ff06f75b88ac574882a2255c507c324254
Elon Demi <elon.demi@student.uni-pr.edu>
2022-05-29 22:26:00 +0200

    changes

--- pages/student/modifiko_profilin.php
+++ pages/student/modifiko_profilin.php
@@ -1,9 +1,12 @@
 <?php
 require "../../resources/parts/header_student.php";
+require_once __DIR__ . "/../../resources/FormHandlers/fjalekalimi/modifiko.php";
 
 
 
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}   
 if (isset($_SESSION['logged_user'])) {
     require_once __DIR__ . "/../../resources/FormHandlers/fjalekalimi/modifiko.php";
     require_once __DIR__ . "/../../resources/query_handler/StudentRepository.php";
@@ -49,10 +52,11 @@ if (isset($_SESSION['logged_user'])) {
         <div style="margin-left:100px;margin-top: 60px;">
             <form action="modifiko_profilin.php?student_id=<?= $students->getStudent_id() ?>" method="post">
                 <h3>Ndrysho Fjalekalimin </h3>
+                <span style="color:red;"> <?php echo $error ?> </br> </span>
                 <label for="fjalekalimi_vjeter">Fjalekalimi i vjeter:</label>
-                <input type="text" id="fjalekalimi_vjeter" name="fjalekalimi_vjeter"><br><br>
+                <input type="password" id="fjalekalimi_vjeter" name="fjalekalimi_vjeter"><br><br>
                 <label for="fjalekalimi_ri">Fjalekalimi i ri:</label>
-                <input type="text" id="fjalekalimi_ri" name="fjalekalimi_ri"><br><br>
+                <input type="password" id="fjalekalimi_ri" name="fjalekalimi_ri"><br><br>
                 <input type="submit" name="modifiko_profilin" value="Ruaj"><br><br>
                 <hr>
             </form>
